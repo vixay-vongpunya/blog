5e4542b4855e7b9bd64bc44731bbd0519fb64053
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-04-07 22:07:28 +0900

    authentication-middleware

---
+++ backend/src/infrastructure/UI/middleware/auth.ts
@@ -0,0 +1,26 @@
+import {Request, Response} from "express"
+import { IUser, IUserToUI } from "@root/src/application/User/domain/IUser";
+import { authUserController } from "@root/DiContainer";
+
+
+declare module 'express-serve-static-core'{
+    interface Request{
+        user?: IUserToUI
+    }
+}
+
+export const authMiddleware = async (req: Request, res: Response, next:any)=>{
+    try{
+        const token = req.headers.authorization?.split('Bearer ')[1]
+        if(!token){
+            res.status(401).json({error: "Credentials are not provided"})
+        }
+
+        req.user = await authUserController.authenticate(token)
+        next()
+    }
+    catch(error:any){
+        res.status(401).json({error:error.message})
+        
+    }
+}
