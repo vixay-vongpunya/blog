c6ad1f6a09a597a5a3c2f9609cb09ce27932ed6e
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-04-11 22:42:31 +0900

    post model updated

--- backend/src/application/Post/domain/Post.ts
+++ backend/src/application/Post/domain/Post.ts
@@ -6,15 +6,15 @@ export class Post implements IPost {
     public id: PostId
     public title: PostTitle
     public content: PostContent
-    public userId: UserId
+    public authorId: UserId
     public created: PostCreated
     public updated: PostUpdated
     
-    constructor(id: PostId, title: PostTitle, content: PostContent, userId: UserId, created: PostCreated, updated: PostUpdated){
+    constructor(id: PostId, title: PostTitle, content: PostContent, authorId: UserId, created: PostCreated, updated: PostUpdated){
         this.id = id;
         this.title = title;
         this.content = content;
-        this.userId = userId;
+        this.authorId = authorId;
         this.created = created;
         this.updated = updated
     }
