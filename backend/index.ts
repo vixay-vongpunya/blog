9b4c20e2981b54afee0dfb6973f58c4414527724
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-04-15 23:47:02 +0900

    add cors

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,16 @@ import "reflect-metadata"
 
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import router from "./src/infrastructure/UI/router";
 
 const app = express();
 dotenv.config();
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));  
 app.use(router);
 
 const PORT = process.env.PORT || 4000;
