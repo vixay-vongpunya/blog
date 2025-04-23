f40bc70351a1020661951c04bb25c22da8cd1cce
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-04-23 22:10:09 +0900

    layouts updated

--- frontend/src/layouts/PageHeader/PageHeader/index.tsx
+++ frontend/src/layouts/PageHeader/PageHeader/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useRouter } from "next/navigation";
 import { navRoutes } from "../../hooks/NavRoutes";
 import { AppBar, Button, MenuItem, Select, Stack, Switch, Toolbar, Typography, useColorScheme} from '@mui/material';
@@ -6,23 +7,23 @@ import TopRight from "../TopRight";
 import { useState } from "react";
 import { Page, PagePath } from "@/providers/PageProviders/hook";
 import PageHeaderBaseCard from "../PageHeaderBaseCard";
-import { useAuth } from "@/providers/AuthProvider";
+
 
 function PageHeader(){
     const router = useRouter();
-    const {authenticated} = useAuth()
     const element = (
-        <>
-        {authenticated ? (
-            <Button variant='contained' onClick={()=>{router.push(PagePath[Page.Edit])}}>Edit</Button>
-        ):(
-            <>
-              <Button variant="contained" onClick={()=>{router.push(PagePath[Page.Login])}}>Log in</Button>
-              <Button variant="outlined" onClick={()=>{router.push(PagePath[Page.SignUp])}}>Sign Up</Button>
-            </>
-              )
-          }
-        </>
+        <Button variant='contained' onClick={()=>{router.push(PagePath[Page.Edit])}}>Edit</Button>
+        // <>
+        // {authenticated ? (
+        //     <Button variant='contained' onClick={()=>{router.push(PagePath[Page.Edit])}}>Edit</Button>
+        // ):(
+        //     <>
+        //       <Button variant="contained" onClick={()=>{router.push(PagePath[Page.Login])}}>Log in</Button>
+        //       <Button variant="outlined" onClick={()=>{router.push(PagePath[Page.SignUp])}}>Sign Up</Button>
+        //     </>
+        //       )
+        //   }
+        // </>
          
     )
     const leftSection = (
